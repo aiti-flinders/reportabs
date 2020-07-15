6c9c77b558556cddc5e45de5e1fd8ab4e8e1094a
Hamish Gamble <gamb0043@flinders.edu.au>
2020-07-15 14:38:56 +0930

    add function to get the next date of release for a cat_no

---
+++ R/abs_release_date.R
@@ -0,0 +1,44 @@
+#' Next Release of an ABS Time Series
+#'
+#' @param cat_no string. Include the .0
+#'
+
+abs_next_release <- function(cat_no) {
+
+  release_url <- glue::glue("https://www.abs.gov.au/AUSSTATS/abs@.nsf/second+level+view?ReadForm&prodno={cat_no}&&tabname=Past%20Future%20Issues")
+
+  release_page <- xml2::read_html(releases_url)
+
+  release_date <- release_page %>%
+    rvest::html_nodes(xpath = '//*[@id="mainpane"]/div/ul[1]/li') %>%
+    html_text()
+
+  next_release <- stringr::str_sub(release_date, start = 13, end = 20)
+
+  next_release <- zoo::as.yearmon(next_release, format = "%b %Y")
+
+  return(next_release)
+
+}
+
+#' Next Release date of an ABS Time Series
+#'
+#' @param cat_no string. include the .0
+
+abs_release_date <- function(cat_no) {
+
+  release_url <- glue::glue("https://www.abs.gov.au/AUSSTATS/abs@.nsf/second+level+view?ReadForm&prodno={cat_no}&&tabname=Past%20Future%20Issues")
+
+  release_page <- xml2::read_html(releases_url)
+
+  release_date <- release_page %>%
+    rvest::html_nodes(xpath = '//*[@id="mainpane"]/div/ul[1]/li') %>%
+    html_text()
+
+  next_date <- stringr::str_sub(release_date, start = -10)
+
+  next_date <- as.Date(next_date, format = "%d/%m/%Y")
+
+  return(next_date)
+
+}
