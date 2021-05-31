26fe416e85c321f454965e53d0a9cd873f9aa1a1
Hamish <hamish.gamble@flinders.edu.au>
2021-05-31 16:11:48 +0930

    Use base functions

--- tests/testthat/test-string_formats.R
+++ tests/testthat/test-string_formats.R
@@ -1,5 +1,3 @@
-library(reportabs)
-library(tibble)
 
 test_that("string formats print the right symbols", {
   expect_match(as_percent(5), "%")
@@ -11,7 +9,7 @@ test_that("string formats print the right symbols", {
 })
 
 test_that("as_comma_group returns a vector the same length as the input", {
-  acg_test <- tribble(~group, ~value, "1", 1, "2", 100, "3", 1000, "4", 1e6)
+  acg_test <- data.frame(group = c("1", "2", "3", "4"), value = c(1, 100, 1000, 1e6))
   expect_equal(length(as_comma_group(acg_test, group = "group", value = "value")), 4)
 })
 
