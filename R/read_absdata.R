cdf56ec7d154831eb37b1c72c64481ee36650c1f
Hamish <hamish.gamble@flinders.edu.au>
2022-02-17 13:38:43 +1030

    Document and export read_absdata

--- R/read_absdata.R
+++ R/read_absdata.R
@@ -1,3 +1,14 @@
+#' Read tidied ABS data from the aitidata GitHub repository.
+#'
+#' @param name The name, as a string, of the dataset to download.
+#' @param refresh FALSE (the default) will load a cached version of the data if it has already been downloaded.
+#' Specify TRUE to force the data to be downloaded again.
+#' @param export_dir The directory to save downloaded data in. Defaults to a temporary directory.
+#' @param .validate_name NYI
+#'
+#' @return data
+#' @export
+#'
 read_absdata <- function(name = NULL,
                          refresh = FALSE,
                          export_dir = tempdir(),
