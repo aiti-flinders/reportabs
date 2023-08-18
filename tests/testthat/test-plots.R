ae42c9272a1b1019b028bfd07d7c1ade19ba306e
Hamish Gamble <hamish.gamble@flinders.edu.au>
2023-08-18 09:35:42 +0930

    Add a test for markdown plots

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -1,4 +1,5 @@
 test_that("plots are plots", {
   expect_s3_class(abs_plot(.data = read_absdata("labour_force"), indicator = "Employed total", states = "South Australia"), "ggplot")
+  expect_s3_class(abs_plot(.data = read_absdata("labour_force"), indicator = "Employed total", states = "South Australia", markdown = TRUE), "ggplot")
 
 })
