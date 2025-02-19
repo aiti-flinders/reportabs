4bbe0ab4917dbe5c6a08e9499742b373c606ade6
Hamish Gamble <hamish.gamble@flinders.edu.au>
2025-02-20 09:42:23 +1030

    Adds missing () to function call using base pipe

--- tests/testthat/test-reporting_functions.R
+++ tests/testthat/test-reporting_functions.R
@@ -5,5 +5,5 @@ test_that("reporting functions return the correct types", {
   expect_type(last_value(lf, filter_list), "character")
   expect_type(change(lf, filter_list), "character")
   expect_match(change(lf, filter_list), "increased|decreased|to")
-  expect_type(average_over(lf, filter_list, as.Date(c("2019-01-01","2020-01-01")), "double")
+  expect_type(average_over(lf, filter_list, as.Date(c("2019-01-01","2020-01-01"))), "double")
 })
