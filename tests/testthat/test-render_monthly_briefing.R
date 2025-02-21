1236d00feabcccc7656f332efcf46d861ebf3e33
Hamish Gamble <hamish.gamble@flinders.edu.au>
2025-02-21 16:50:27 +1030

    Change directory

--- tests/testthat/test-render_monthly_briefing.R
+++ tests/testthat/test-render_monthly_briefing.R
@@ -1,4 +1,4 @@
 test_that("pdf output works", {
   expect_type(object = render_monthly_briefing(output_format = "html"), type = "NULL")
-  file.remove("tests/testthat/out/2025-january-south-australia.html")
+  file.remove("2025-january-south-australia.html")
 })
