97636ac6c3ce66b7169c7d4547b3889be3745f06
Hamish Gamble <hamish.gamble@flinders.edu.au>
2025-04-17 10:49:25 +0930

    Updates tests

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -1,6 +1,45 @@
-test_that("plots are plots", {
-  expect_s3_class(abs_plot(data = read_absdata("labour_force"),
-                           over = list(indicator = "Employed total", state = "South Australia")),
-                  "ggplot")
+test_that("Plots can be generated", {
+  library(reportabs)
+  plot_data <- read_absdata("labour_force_briefing")
+
+
+  expect_s3_class(
+    abs_plot(labour_force_briefing,
+             list(indicator = "Employed total",
+                  series_type = "Seasonally Adjusted",
+                  state = "South Australia")),
+    "ggplot")
+  expect_s3_class(
+    abs_plot(labour_force_briefing,
+             list(indicator = "Employed total",
+                  series_type = "Seasonally Adjusted",
+                  state = "South Australia"),
+             compare_aus = FALSE),
+    "ggplot")
+  expect_s3_class(
+    abs_plot(labour_force_briefing,
+             list(indicator = c("Employed total", "Employed full-time"),
+                  series_type = "Seasonally Adjusted",
+                  state = "Australia"),
+             compare_aus = FALSE),
+    "ggplot")
+
+  # Specifying multiple indicators and multiple other variables shouldn't work unless a facet has been specified
+  expect_error(
+    abs_plot(labour_force_briefing,
+             list(indicator = c("Employed total", "Employed full-time"),
+                  series_type = "Seasonally Adjusted",
+                  state = "South Australia")
+    )
+  )
+
+  expect_s3_class(
+    abs_plot(labour_force_briefing,
+             list(indicator = c("Employed total", "Employed full-time"),
+                  series_type = "Seasonally Adjusted",
+                  state = "South Australia"),
+             facet = "state"
+  ),
+  "ggplot")
 
 })
--- tests/testthat/test-string_formats.R
+++ tests/testthat/test-string_formats.R
@@ -8,8 +8,15 @@ test_that("string formats print the right symbols", {
   expect_match(as_comma(1e9), ",| million")
 })
 
-test_that("as_comma_group returns a vector the same length as the input", {
+test_that("as_comma_group returns a correctly formatted vector", {
   acg_test <- data.frame(group = c("1", "2", "3", "4"), value = c(1, 100, 1000, 1e6))
   expect_equal(length(as_comma_group(acg_test, group = "group", value = "value")), 4)
+  expect_equal(ignore_attr = TRUE, as_comma_group(acg_test, group = "group", value = "value"), c("1", "100", "1,000", "1.0 million"))
+  expect_equal(ignore_attr = TRUE, as_comma_group(acg_test, group = "group", value = "value"), as_comma(acg_test, group = "group", value = "value"))
+})
+
+test_that("guard rails prevent bad arguments", {
+  expect_error(as_comma(5, value = "value"))
+
 })
 
